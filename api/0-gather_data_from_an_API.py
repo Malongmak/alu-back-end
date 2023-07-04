81f1b12f5228d13afe3e8d6bbb814a36abd9b267
Malongmak <j.malong@alustudent.com>
2023-07-04 16:48:25 +0300

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
+"""
+This script retrieves information about an employee's TODO list progress using a REST API.
+
+Usage:
+    python3 script_name.py employee_id
+
+Arguments:
+    employee_id (int): The ID of the employee to fetch TODO list progress for.
+
+Example:
+    python3 script_name.py 123
+"""
+
 import requests
 
 def get_employee_todo_progress(employee_id):
+    """
+    Fetches the TODO list progress for the given employee ID and displays it on the standard output.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        None
+    """
     # Make a GET request to the API
     response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
@@ -24,9 +46,17 @@ def get_employee_todo_progress(employee_id):
     else:
         print("Failed to retrieve TODO list.")
 
-# Prompt for employee ID
-employee_id = int(input("Enter employee ID: "))
+if __name__ == '__main__':
+    import sys
 
-# Call the function to get employee's TODO list progress
-get_employee_todo_progress(employee_id)
+    # Check if an employee ID is provided as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py employee_id")
+        sys.exit(1)
+
+    # Get the employee ID from the command-line argument
+    employee_id = int(sys.argv[1])
+
+    # Call the function to get employee's TODO list progress
+    get_employee_todo_progress(employee_id)
 
