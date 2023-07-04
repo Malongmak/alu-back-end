781f393eba2ef3ea9b5372da65f53be8e29e54a9
Malongmak <j.malong@alustudent.com>
2023-07-04 23:07:33 +0300

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-import requests
+import request
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-    
-    # Retrieve employee information
-    response = requests.get(employee_url)
-    if response.status_code != 200:
-        print(f"Error: Could not retrieve employee information for ID {employee_id}")
-        return
-    
-    employee_data = response.json()
-    employee_name = employee_data.get("name")
-    
-    # Retrieve TODO list information
-    response = requests.get(todo_url)
-    if response.status_code != 200:
-        print(f"Error: Could not retrieve TODO list for employee ID {employee_id}")
-        return
-    
-    todo_data = response.json()
-    total_tasks = len(todo_data)
-    done_tasks = [task for task in todo_data if task.get("completed")]
-    num_done_tasks = len(done_tasks)
-    
-    # Print the progress information
-    print(f"Employee {employee_name} is done with tasks ({num_done_tasks}/{total_tasks}):")
-    
-    for task in done_tasks:
-        print(f"\t{task.get('title')}")
+BASE_URL = "https://jsonplaceholder.typicode.com/todos"
 
-# Example usage
-employee_id = 1
-get_employee_todo_progress(employee_id)
+def get_todo_list_progress(employee_id):
+    response = requests.get(f"{BASE_URL}/{employee_id}")
+
+    if response.status_code == 200:
+        data = response.json()
+
+        number_of_completed_tasks = len([task for task in data if task["completed"]])
+
+        total_number_of_tasks = len(data)
+
+        print(f"Employee {data['name']} is done with tasks({number_of_completed_tasks}/{total_number_of_tasks}):")
+
+        for task in data:
+            if task["completed"]:
+                print("\t", task["title"])
+
+    else:
+        print("Error getting TODO list progress.")
+
+employee_id = int(input("Enter the employee ID: "))
+
+get_todo_list_progress(employee_id)
